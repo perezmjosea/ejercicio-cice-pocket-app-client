6b8c96a48bce71bb01fad10904e4925b0cabb8b3
GonzaloCedillo <goncedillo@gmail.com>
2018-10-06 14:52:31 +0200

    Montando area de creación

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,12 @@
 // Inicializamos componentes Materialize
 M.AutoInit();
+
+// Self Invoked Function (contexto privado)
+(function() {
+  const $actionBtn = document.querySelector("#action-btn");
+  const $creationArea = document.querySelector("#creation-area");
+
+  $actionBtn.addEventListener("click", e => {
+    $creationArea.classList.add("open");
+  });
+})();
