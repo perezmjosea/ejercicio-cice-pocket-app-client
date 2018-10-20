41137f7a1f8d86078d9243aaf77b0a6797df602a
GonzaloCedillo <goncedillo@gmail.com>
2018-10-20 12:10:34 +0200

    Controloando estados del envío

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,10 @@ M.AutoInit();
 
 // Self Invoked Function (contexto privado)
 (function () {
+  // Expresión regular que comprueba una URL válida
+  const urlRegExp = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm);
+
+  // Card para pintar
   const card = `<div class="col s12 m6 l4">
   <div class="card">
     <div class="card-content">
@@ -16,22 +20,55 @@ M.AutoInit();
   </div>
 </div>`;
 
-
   const $actionBtn = document.querySelector("#action-btn");
   const $creationArea = document.querySelector("#creation-area");
   const $closeCreationArea = document.querySelector("#close-creation-area");
   const $createWesiteForm = document.querySelector("#create-website-form");
   const $websitesGrid = document.querySelector("#websites-grid");
   const $urlWebsite = document.querySelector("#url-website");
+  const $submitWebsiteBtn = document.querySelector("#submit-website-btn");
 
+  let isSendingWebsite = false;
+
+
+  ////////////////////////////////////
+  //// Helper functions
+  ////
 
   function clearCreationArea() {
     $urlWebsite.value = "";
+
+    isSendingWebsite = false;
+
+    $closeCreationArea.classList.remove("loading");
+    $submitWebsiteBtn.classList.remove("disabled");
+    $urlWebsite.classList.add("validate");
+    $urlWebsite.removeAttribute("disabled");
+
     $creationArea.classList.remove("open");
   }
 
+  function websiteCreationSuccess() {
+    // Limpiamos y cerramos el área de creación
+    clearCreationArea();
+
+    $websitesGrid.innerHTML += card;
+  }
+
+  function websiteCreationFail() {
+    // Limpiamos y cerramos el área de creación
+    clearCreationArea();
+
+    M.toast({
+      html: "Algo ha ido mal en tu envío",
+      classes: "red-text text-lighten-3"
+    });
+  }
 
 
+  ////////////////////////////////////
+  //// Handlers
+  ////
 
   // Click para abrir zona de creación
   $actionBtn.addEventListener("click", e => {
@@ -40,16 +77,28 @@ M.AutoInit();
 
   // Click para cerrar zona de creación
   $closeCreationArea.addEventListener("click", e => {
-    $creationArea.classList.remove("open");
+    if (!isSendingWebsite) {
+      $creationArea.classList.remove("open");
+      clearCreationArea();
+    }
   });
 
   // Control del submit del formulario de creación
   $createWesiteForm.addEventListener("submit", e => {
     e.preventDefault();
 
-    // Limpiamos y cerramos el área de creación
-    clearCreationArea();
+    // 1. Validar el texto como url válida
+    if (!urlRegExp.test($urlWebsite.value)) {
+      return;
+    }
 
-    $websitesGrid.innerHTML += card;
+    isSendingWebsite = true;
+
+    $closeCreationArea.classList.add("loading");
+    $submitWebsiteBtn.classList.add("disabled");
+    $urlWebsite.classList.remove("validate");
+    $urlWebsite.setAttribute("disabled", "disabled");
+
+    return;
   });
 })();
