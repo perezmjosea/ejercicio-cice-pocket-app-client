3eb7113112a237e304fe21e1c879242d43626e2d
perezm.josea <perezm.josea@gmail.com>
2018-10-27 15:02:41 +0200

    Filters & Categories

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,33 +2,42 @@
 M.AutoInit();
 
 // Self Invoked Function (contexto privado)
-(function () {
+(function() {
   // Expresión regular que comprueba una URL válida
-  const urlRegExp = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm);
+  const urlRegExp = new RegExp(
+    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm
+  );
 
-  const SERVER_URL = "http://localhost:3001";
+  const SERVER_URL = "http://localhost:3003";
 
+  const $navMobile = document.querySelector("#nav-mobile");
   const $actionBtn = document.querySelector("#action-btn");
   const $creationArea = document.querySelector("#creation-area");
   const $closeCreationArea = document.querySelector("#close-creation-area");
   const $createWesiteForm = document.querySelector("#create-website-form");
   const $websitesGrid = document.querySelector("#websites-grid");
   const $urlWebsite = document.querySelector("#url-website");
+  // const $categoriesList = document.querySelector("#categories-list");
   const $submitWebsiteBtn = document.querySelector("#submit-website-btn");
 
   let isSendingWebsite = false;
-
 
   ////////////////////////////////////
   //// Helper functions
   ////
 
   // Card para pintar
-  const getCard = ({ title, url, description, image }) => `<div class="col s12 m6 l4">
+  const getCard = ({
+    title,
+    url,
+    description,
+    keywords,
+    image
+  }) => `<div class="col s12 m6 l4">
   <div class="card">
       <div class="card-image">
         <img src="${SERVER_URL}/${image}">
-        </div>
+      </div>
       <div class="card-content">
         <span class="card-title black-text">${title}</span>
         <p>${description}</p>
@@ -38,6 +47,10 @@ M.AutoInit();
       </div>
     </div>
   </div>`;
+
+  const getCategory = ({ _id, name }) => `
+  <option value="${_id}">${name}</option>
+  `;
 
   function clearCreationArea() {
     $urlWebsite.value = "";
@@ -70,12 +83,59 @@ M.AutoInit();
   }
 
   async function getInitialCards() {
-    const req = await fetch(`${SERVER_URL}/website/list`).catch(err => websiteCreationFail());
+    const req = await fetch(`${SERVER_URL}/website/list`).catch(err =>
+      websiteCreationFail()
+    );
     const data = await req.json();
 
-    data.list && data.list.forEach(item => $websitesGrid.innerHTML += getCard(item));
+    data.list &&
+      data.list.forEach(item => ($websitesGrid.innerHTML += getCard(item)));
   }
 
+  async function getInitialCategories() {
+    const req = await fetch(`${SERVER_URL}/website/categories`).catch(err =>
+      websiteCreationFail()
+    );
+    const data = await req.json();
+
+    if (!data) {
+      websiteCreationFail();
+      return;
+    }
+
+    let categoriesSelect = `
+      <select id="categories-list" name="category">
+      <option value="" disabled selected>Choose website category</option>
+    `;
+    data.categories.forEach(item => {
+      categoriesSelect += `
+        <option value="${item._id}">${item.name}</option>
+      `;
+
+      $navMobile.innerHTML += `
+      <li>
+        <a href="#" data-id="${item._id}">${item.name}</a>
+      </li>
+      `;
+    });
+    categoriesSelect += `
+        <label>Categories</label>
+      </select>
+    `;
+
+    const $categoriesField = document.querySelector("#categories-field");
+    $categoriesField.innerHTML += categoriesSelect;
+
+    const $categoriesList = document.querySelector("#categories-list");
+    M.FormSelect.init($categoriesList);
+    // categoriesInstance.updateData();
+    // M.FormSelect.init($categoriesList, data.categories);
+
+    // data.categories &&
+    //   data.categories.forEach(
+    //     item => ($categoriesList.innerHTML += getCategory(item))
+    //   );
+  }
 
   ////////////////////////////////////
   //// Handlers
@@ -112,6 +172,10 @@ M.AutoInit();
     $urlWebsite.classList.remove("validate");
     $urlWebsite.setAttribute("disabled", "disabled");
 
+    const $categoriesList = document.querySelector("#categories-list");
+    const instanceCat = M.FormSelect.getInstance($categoriesList);
+    const category = instanceCat.getSelectedValues();
+
     // Preparo cabeceras y petición de envío
     const reqOptions = {
       method: "POST",
@@ -119,11 +183,13 @@ M.AutoInit();
         "Content-Type": "application/json"
       },
       mode: "cors",
-      body: JSON.stringify({ url: $urlWebsite.value })
+      body: JSON.stringify({ url: $urlWebsite.value, category: category })
     };
 
     // Envío + catch
-    const req = await fetch(`${SERVER_URL}/website/create`, reqOptions).catch(err => websiteCreationFail());
+    const req = await fetch(`${SERVER_URL}/website/create`, reqOptions).catch(
+      err => websiteCreationFail()
+    );
 
     // Si la respuesta no es creación, genera error
     if (req.status.toString() !== "201") {
@@ -137,10 +203,10 @@ M.AutoInit();
     websiteCreationSuccess(resp);
   });
 
-
   ///////////////////////////////////
   //// Scripts de inicio
   ////
 
   getInitialCards();
+  getInitialCategories();
 })();
